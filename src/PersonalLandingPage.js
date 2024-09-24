44a0dbe0c1721dbacf5478a8985d38547d3d9d77
Teo Ivančević <teo@tomsoft.hr>
2024-09-24 22:19:48 +0200

    formatting title

--- src/PersonalLandingPage.js
+++ src/PersonalLandingPage.js
@@ -6,7 +6,7 @@ const PersonalLandingPage = () => {
     const [email, setEmail] = useState('');
     const [isSubmitted, setIsSubmitted] = useState(false);
   
-    const FORMSPARK_ACTION_URL = "https://submit-form.com/iFdC8wLoM"; // Replace with your Formspark form ID
+    const FORMSPARK_ACTION_URL = "https://submit-form.com/iFdC8wLoM"; 
   
     const handleSubmit = async (e) => {
       e.preventDefault();
@@ -25,10 +25,10 @@ const PersonalLandingPage = () => {
     };
 
   return (
-    <div className="min-h-screen bg-[#ffffff] flex justify-left p-10 ">
+    <div className="min-h-screen bg-[#ffffff] flex justify-left p-8 ">
       <div className="w-full max-w-[600px] flex flex-col justify-between">
         <header className="mb-8">
-          <h1 className="text-5xl font-bold text-[#333333]">
+          <h1 className="text-3xl font-bold text-[#001122]">
             Hi, I'm <strong><span className="bg-gradient-to-r from-green-400 to-teal-400 text-transparent bg-clip-text">Teo</span></strong> Ivancevic
             {/* Hi, I'm <span className="bg-gradient-to-r from-green-400 to-teal-400 text-transparent bg-clip-text">Teo</span> <span className="bg-gradient-to-r to-teal-500 from-teal-400 text-transparent bg-clip-text">Ivančević</span>. */}
             
@@ -39,16 +39,16 @@ const PersonalLandingPage = () => {
         </header>
 
         <main className="flex-grow text-xl">
-          <section className="mb-6">
+          <section className="mb-12">
             {/* <h2 className="text-lg font-semibold mb-2 text-[#666666]">About Me</h2> */}
-            <p className="text-[#666666]">
+            <p className="text-[#888888]">
               I love Startups, Technology, Music, Business, Robotics...
               <br/><br/>
               Currently, I'm studying CS at <a href='https://www.fer.unizg.hr/en' className='underline'>FER</a> in <strong>Zagreb, Croatia</strong>. 
               {/* I am working on starting an Entrepreneurship Club at my uni to "Inspire the new generation of Croatian leaders". */}
               <br/><br/>
               Recently, I've spent 2 months at <a href='https://www.stanford.edu/' className='font-bold hover:text-[#8C1515]'>Stanford University</a> as a visiting student for the summer. While studying Technology Entrepreneurship and Data Science I've made a lot of cool friends from all around the world. 
-              <br/><br/>
+              
               {/* Some of my favorite songs lately:
               <ol className="text-[#666666] list-decimal pl-10">
                 <li><a href='https://www.youtube.com/watch?v=e-IAGmTuUmw' className='underline'>The Glory</a> by Ye</li>
@@ -60,7 +60,7 @@ const PersonalLandingPage = () => {
           
           <section className="mb-6">
                 {/* <h2 className="text-lg font-semibold mb-2 text-[#444444]">Newsletter</h2> */}
-                <p className="text-[#666666]">
+                <p className="text-[#888888]">
                     Also, I started writing <a href='https://teoivancevic.substack.com' className='font-bold underline'>mini blog posts</a> about stuff I find cool, my experiences or just my thoughts. 
                     <br/>
                     I write maybe <strong>1 per month</strong>, so if you want to hear from me you can add yourself to my list
@@ -96,9 +96,9 @@ const PersonalLandingPage = () => {
           </section>
           
         </main>
-        <footer className="mt-8 pt-4 text-[#666666] text-xl">
+        <footer className="text-[#888888] text-xl">
           <section>
-            <p className="text-[#666666]">
+            <p className="text-[#888888]">
                 p.s: I like getting emails, so if you want to talk about startups, tech, or literally anything, <a href="mailto:hey@teoivancevic.com" aria-label="Email" className='underline'>hit me up</a> :)
             </p>
             {/* <br/>
@@ -108,7 +108,7 @@ const PersonalLandingPage = () => {
           
           </section>
         </footer>
-        <footer className="mt-8 pt-4 border-t border-[#dddddd] text-[#666666]">
+        <footer className="mt-8 pt-4 border-t border-[#dddddd] text-[#888888]">
           <div className="flex justify-between items-center mb-2">
             {/* <p className="text-sm">&copy; 2024 Teo Ivancevic</p> */}
             
@@ -120,13 +120,13 @@ const PersonalLandingPage = () => {
                 <Mail className="text-[#666666] hover:text-[#444444]" size={20} />
               </a> */}
               <a href="https://cal.com/teoivancevic" target="_blank" rel="noopener noreferrer" aria-label="Cal.com Meeting">
-                <CalendarCheck className="text-[#666666] hover:text-[#9834eb]" size={20} />
+                <CalendarCheck className="text-[#888888] hover:text-[#9834eb]" size={20} />
               </a>
               <a href="https://teoivancevic.substack.com" target="_blank" rel="noopener noreferrer" aria-label="Substack Newsletter">
-                <Rss className="text-[#666666] hover:text-[#FF6719]" size={20} />
+                <Rss className="text-[#888888] hover:text-[#FF6719]" size={20} />
               </a>
               <a href="https://www.linkedin.com/in/teoivancevic" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn Profile">
-                <Linkedin className="text-[#666666] hover:text-[#0077B5]" size={20} />
+                <Linkedin className="text-[#888888] hover:text-[#0077B5]" size={20} />
               </a>
             </div>
             
